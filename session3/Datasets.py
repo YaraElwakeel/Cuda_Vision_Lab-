813fc7a1daa63eb5cec6dfce9aba3a70dabc3b1c
YaraElwakeel <yaraelwakeel010@gmail.com>
2024-11-15 17:17:43 +0100

    data_loader added

---
+++ session3/Datasets.py
@@ -0,0 +1,50 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
+
+
+class dataset(Dataset):
+    def __init__(self, root, transform=None,split="train", test_size=0.2, random_state=42):
+        self.root_dir = root
+        self.transform = transform
+        self.split = split
+
+        self.image_paths = []
+        self.labels = []
+        
+        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(sorted(os.listdir(root)))}
+        
+        for cls_name, idx in self.class_to_idx.items():
+            cls_folder = os.path.join(root, cls_name)
+            for img_name in os.listdir(cls_folder):
+                self.image_paths.append(os.path.join(cls_folder, img_name))
+                self.labels.append(idx)
+        train_paths, test_paths, train_labels, test_labels = train_test_split(
+                self.image_paths, self.labels, test_size=test_size, random_state=random_state, stratify=self.labels
+            )
+        # Select the subset based on the split argument
+        if self.split == "train":
+            self.image_paths = train_paths
+            self.labels = train_labels
+        elif self.split == "test":
+            self.image_paths = test_paths
+            self.labels = test_labels
+        else:
+            raise ValueError("Invalid split! Use 'train' or 'test'.")
+    
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        label = self.labels[idx]
+        
+        # Load the image
+        image = Image.open(img_path).convert("RGB")
+        
+        # Apply transformations if specified
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
---
+++ session3/Model_Wrapper.py
@@ -0,0 +1,162 @@
+import torch
+import numpy as np
+import seaborn as sn
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+
+class Wrapper():
+    def __init__(self, model, device, criterion, optimizer, scheduler=None, warmup_lr=None):
+        # Track training and testing loss and accuracy for each epoch
+        self.loss_hist = [] 
+        self.acc_hist = []
+        self.loss_test_hist = []
+        self.acc_test_hist = []
+
+        # Store scheduler and warmup learning rate if provided
+        self.scheduler = scheduler
+        self.warmup_lr = warmup_lr
+        
+        # Initialize lists for predictions and true labels for confusion matrix
+        self.predictions = []
+        self.true_labels = []
+        
+        # Define model parameters
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.model = model
+        self.device = device
+
+        # Initialize other attributes to None for later setup
+        self.classes = None
+        self.testloader = None
+        self.trainloader = None
+
+    def train(self, num_epochs, trainloader, testloader, classes):
+        # Set classes and loaders
+        self.classes = classes
+        self.trainloader = trainloader
+        self.testloader = testloader
+
+        for epoch in range(num_epochs):
+            correct = 0 
+            total = 0 
+            loss_list = []
+            acc_list = []
+
+            # Reset predictions and true_labels for this epoch
+            self.predictions = []
+            self.true_labels = []
+
+            # Display a progress bar for each epoch
+            progress_bar = tqdm(self.trainloader, total=len(self.trainloader))
+            for i, (inputs, labels) in enumerate(progress_bar):
+                self.model.train()
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                
+                # Forward pass
+                self.optimizer.zero_grad()
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                loss_list.append(loss.item())
+                
+                # Backpropagation and optimizer step
+                loss.backward()
+                self.optimizer.step()
+                
+                # Apply learning rate scheduling if provided
+                if self.warmup_lr:
+                    self.warmup_lr.step(epoch)
+                if self.scheduler:
+                    self.scheduler.step(T_curr=epoch, T_max=num_epochs)
+
+                # Calculate accuracy for current batch
+                preds = torch.argmax(outputs, dim=1)
+                correct += (preds == labels).sum().item()
+                total += labels.size(0)
+                accuracy = 100 * correct / total
+                acc_list.append(accuracy)
+
+                # Update progress bar description every 10 batches
+                if (i % 10 == 0 or i == len(self.trainloader) - 1):
+                    progress_bar.set_description(f"Epoch {epoch + 1} Iter {i + 1}: loss {loss.item():.5f}.")
+            
+            # Log epoch loss and accuracy
+            self.loss_hist.append(np.mean(loss_list))
+            self.acc_hist.append(np.mean(acc_list))
+
+            # Evaluate on the test data and log test metrics
+            loss_test_list, test_accuracy, epoch_predictions, epoch_true_labels = self.eval()
+            self.loss_test_hist.append(np.mean(loss_test_list))
+            self.acc_test_hist.append(test_accuracy)
+
+            # Store predictions and true labels for confusion matrix
+            self.predictions.extend(epoch_predictions)
+            self.true_labels.extend(epoch_true_labels)
+
+    def eval(self):
+        correct = 0
+        total = 0
+        true_labels = []
+        predictions = []
+        loss_list = []
+        
+        # Switch to evaluation mode
+        self.model.eval()
+        with torch.no_grad():
+            for inputs, labels in self.testloader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.model(inputs)
+
+                # Compute loss for this batch
+                loss = self.criterion(outputs, labels)
+                loss_list.append(loss.item())
+
+                # Compute accuracy for this batch
+                _, predicted = torch.max(outputs, dim=1)
+                correct += (predicted == labels).sum().item()
+                total += labels.size(0)
+                predictions.extend(predicted.cpu().numpy())
+                true_labels.extend(labels.cpu().numpy())
+
+        # Calculate the overall test accuracy
+        accuracy = 100 * correct / total
+        return loss_list, accuracy, predictions, true_labels
+
+    def confusion_matrix(self):
+        # Check if predictions and true_labels are available
+        if self.true_labels and self.predictions:        
+            # Generate and display the confusion matrix
+            cf_matrix = confusion_matrix(self.true_labels, self.predictions)
+            sn.heatmap(cf_matrix, annot=True, fmt="d", xticklabels=self.classes, yticklabels=self.classes)
+
+            plt.figure(figsize=(12, 7))
+            plt.savefig('output.png')
+        else:
+            print("No predictions available. Please run `train` first.")
+    
+    def valid_accuracy (self):
+            # Print validation accuracy
+            accuracy = accuracy_score(self.true_labels, self.predictions) * 100
+            print(f"Validation accuracy: {round(accuracy, 2)}%")
+
+    def plot_loss_acc(self):
+        # Create subplots for loss and accuracy
+        fig, ax = plt.subplots(1, 2)
+        fig.set_size_inches(16, 5)
+
+        # Plot training and testing loss
+        ax[0].plot(self.loss_hist, label="Train Loss", color="blue", linewidth=3)
+        ax[0].plot(self.loss_test_hist, color="red", label="Test Loss", linewidth=3)
+        ax[0].set_xlabel("Epochs")
+        ax[0].set_ylabel("Loss")
+        ax[0].legend(loc="best")
+
+        # Plot training and testing accuracy
+        ax[1].plot(self.acc_hist, label="Train Accuracy", color="blue", linewidth=3)
+        ax[1].plot(self.acc_test_hist, color="red", label="Test Accuracy", linewidth=3)
+        ax[1].set_xlabel("Epochs")
+        ax[1].set_ylabel("Accuracy")
+        ax[1].legend(loc="best")
+
+        plt.show()

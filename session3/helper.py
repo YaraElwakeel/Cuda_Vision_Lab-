1fe56e2718dd54c7d568b088b70538e1787dfe08
YaraElwakeel <yaraelwakeel010@gmail.com>
2024-11-18 22:01:33 +0100

    _

--- session3/helper.py
+++ session3/helper.py
@@ -30,9 +30,6 @@ def set_random_seed(random_seed=None):
 def show_grid(data, titles=None):
     """Imshow for Tensor."""
     data = data.numpy().transpose((0, 2, 3, 1))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    data = std * data + mean 
     data = np.clip(data, 0, 1)
     
     plt.figure(figsize=(8*2, 4*2))
